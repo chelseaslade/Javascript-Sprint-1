9a151cec2c31b70022c7c39fbeb6dfd858eecc7a
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-29 10:01:47 -0230

    Pages added, render restaurant menu page on get menu, menu alerts link

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
 const { Restaurants, Cuisines } = require("./utils/data");
-const express = require('express');
-const path = require('path');
-const { generateRandomMenuItem, generateMenu, selectRandomCuisine } = require("./utils/restaurantUtils");
+const express = require("express");
+const path = require("path");
+const {
+  generateRandomMenuItem,
+  generateMenu,
+  selectRandomCuisine,
+} = require("./utils/restaurantUtils");
 
 const app = express();
 let restaurantData = {}; //This should be populated soon
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 
 /**
  * GET /
  * Renders the homepage that lists cities and restaurant names.
  */
-app.get('/', (request, response) => {
-    response.render('index', { restaurants: Restaurants });
-  });
-  
-  /**
-   * GET /restaurant/:name
-   * Displays a specific restaurant's random menu.
-   * The cuisine is randomly selected and a menu is generated based on it.
-   */
-  app.get('/restaurant', (request, response) => {
-    const restaurantId = request.query.restaurantId;
-    console.log(`restaurantId: ${restaurantId}`);
-    //Get the restaurants menu, and then display the page
-  });
+app.get("/", (request, response) => {
+  response.render("index", { restaurants: Restaurants });
+});
 
-  //Add any other required routes here
+/**
+ * GET /restaurant/:name
+ * Displays a specific restaurant's random menu.
+ * The cuisine is randomly selected and a menu is generated based on it.
+ */
+app.get("/restaurant", (request, response) => {
+  const restaurantId = request.query.restaurantId;
+  console.log(`restaurantId: ${restaurantId}`);
+
+  //Display page
+  response.render("restaurantmenu", { restaurantId });
+});
+
+//Add any other required routes here
+//Menu Alerts
+app.get("/menualerts", (request, response) => {
+  response.render("menualerts");
+});
+
+app.get("/restaurantmenu", (request, response) => {
+  response.render("restaurantmenu");
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
